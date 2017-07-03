7d1db82a3c7e2bae11333d3230873127dd342d20
Vlad Stoian <vstoian@pivotal.io>
2017-07-03 07:06:03 +0100

    Add more arguments
    Use --dry-run for now

--- weight.py
+++ weight.py
@@ -2,11 +2,18 @@ import argparse
 import os
 import tempfile
 import tarfile
+import datadog
 from zipfile import ZipFile
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("file_path", help="path to your .pivotal file")
+parser.add_argument("--file-path", help="path to your .pivotal file")
+
+parser.add_argument("--api-key", help="datadog api key")
+parser.add_argument("--app-key", help="datadog app key")
+
+parser.add_argument("--dry-run", help="only print metrics to stdout", action='store_true')
+
 args = parser.parse_args()
 
 metrics_prefix = "vstoian.test.1"
@@ -20,6 +27,8 @@ temp_dir = tempfile.mkdtemp()
 product_name = ""
 product_version = ""
 product_size = os.path.getsize(args.file_path)
+
+product_modification_time = os.path.getmtime(args.file_path)
 
 product_releases = []
 
@@ -69,10 +78,35 @@ with tempfile.TemporaryDirectory() as temp_dir:
 # print(product_version)
 # print(product_size)
 # print(product_releases)
+# print(product_creation_time)
 
 
-print("dog metric post --host {} --tags version:{} --type gauge {}.{}.size {}".format(metrics_host, product_version, metrics_prefix, product_name, product_size))
+if args.dry_run:
+    print("Dry run activated, no metrics are sent to DataDog")
+    print("dog metric post --host {} --tags version:{} --type gauge {}.{}.size {}".format(metrics_host, product_version, metrics_prefix, product_name, product_size))
 
-for release in product_releases:
-    print("dog metric post --host {} --tags release:{},version:{} --type gauge {}.{}.releases {}".format(metrics_host, release['name'], release['version'], metrics_prefix, product_name, release['size']))
+    for release in product_releases:
+        print("dog metric post --host {} --tags release:{},version:{} --type gauge {}.{}.releases {}".format(metrics_host, release['name'], release['version'], metrics_prefix, product_name, release['size']))
+else:
+    datadog.initialize(api_key=args.api_key, app_key=args.app_key, host_name=metrics_host)
+
+    product_size_metric = "{}.{}.size".format(metrics_prefix, product_name)
+    product_size_tags = [ "version:{}".format(product_version) ]
+
+    # print(product_size_metric)
+    # print(product_size_tags)
+
+    stats = datadog.ThreadStats()
+    stats.start()
+
+    stats.gauge(product_size_metric, product_size, product_modification_time, product_size_tags)
+    for release in product_releases:
+        release_metric = "{}.{}.releases".format(metrics_prefix, product_name)
+        release_tags = [ "release:{}".format(release['name']), "version:{}".format(release['version'])]
+
+        # print(release_metric)
+        # print(release_tags)
+
+        stats.gauge(release_metric, release['size'], product_modification_time, release_tags)
+
 
